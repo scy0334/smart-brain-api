c36b273b4c02f80b60e1c70d3e881470a9219577
Si Choi <sichoi@Siui-MacBook-Pro.local>
2020-04-29 18:52:30 +0900

    updating datebase

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controller/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
+        host : 'postgresql-perpendicular-43379',
         user : 'sichoi',
         password : '',
         database : 'smart-brain'
